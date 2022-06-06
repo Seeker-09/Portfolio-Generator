e1f521cb7a52ed6de55108c32470f75d9fb5b1e5
Seeker <54086931+Seeker-09@users.noreply.github.com>
2022-06-06 13:21:26 -0400

    added confirmations to certain elements

--- app.js
+++ app.js
@@ -5,7 +5,16 @@ const promptUser = () => {
         {
             type: 'input',
             name: 'name',
-            message: 'What is your name?'
+            message: 'What is your name?',
+            validate: nameInput => {
+                if(nameInput) {
+                    return true;
+                }
+                else {
+                    console.log('Please enter your name');
+                    return false;
+                }
+            }
         },
         {
             type: 'input',
@@ -35,12 +44,30 @@ const promptProject = portfolioData => {
         {
             type: 'input',
             name: 'name',
-            message: 'What is the name of your project'
+            message: 'What is the name of your project',
+            validate: nameInput => {
+                if(nameInput) {
+                    return true;
+                }
+                else {
+                    console.log('Please enter your name');
+                    return false;
+                }
+            }
         },
         {
             type: 'input',
             name: 'description', 
-            message: 'Provide a description of the project'
+            message: 'Provide a description of the project',
+            validate: nameInput => {
+                if(nameInput) {
+                    return true;
+                }
+                else {
+                    console.log('Please enter your name');
+                    return false;
+                }
+            }
         },
         {
             type: 'checkbox',
@@ -51,7 +78,16 @@ const promptProject = portfolioData => {
         {
             type: 'input',
             name: 'link',
-            message: 'Enter the Github link to your project. (Required)'
+            message: 'Enter the Github link to your project. (Required)',
+            validate: nameInput => {
+                if(nameInput) {
+                    return true;
+                }
+                else {
+                    console.log('Please enter your name');
+                    return false;
+                }
+            }
         },
         {
             type: 'confirm',
